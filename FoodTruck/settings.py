4f430c6ac1dd9eccbbe1559c6826c69e852c847f
kadamvish <vishakha.kad@gmail.com>
2018-01-21 18:36:52 +0530

    Minor changes

--- FoodTruck/settings.py
+++ FoodTruck/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = 'F:/FoodTruck/frontend'
+# MEDIA_ROOT = 'F:/FoodTruck/frontend'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'frontend')
 STATICFILES_DIRS = (
-  'F:/FoodTruck/frontend',
+   os.path.join(BASE_DIR, 'frontend'),
 )
